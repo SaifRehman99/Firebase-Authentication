0c3d1c94db15a6e583be2670477e6ca9bca484db
SaifRehman99 <saifr7493@gmail.com>
2019-05-17 20:01:25 +0500

    firebase SDk added

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+    //  https://firebase.google.com/docs/web/setup#config-web-app -->
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCjkfvYeudQvegto1XRSUW5i5LSpa1aeJY",
+    authDomain: "authentication-36e16.firebaseapp.com",
+    databaseURL: "https://authentication-36e16.firebaseio.com",
+    projectId: "authentication-36e16",
+    storageBucket: "authentication-36e16.appspot.com",
+    messagingSenderId: "1012143452343",
+    appId: "1:1012143452343:web:e9a3984f22f57961"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
